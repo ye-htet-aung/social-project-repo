bcac53c83e07218256e5c2a9abaaea758d7dd69e
Tun Aung Lin <gangarramghimire@gmail.com>
2025-03-01 01:09:08 +0630

    addpost

--- screen/Home.php
+++ screen/Home.php
@@ -5,26 +5,26 @@
 <div id="main">
         <div id="menu"></div>
         <div id="media">
+            <div id="addnewpost">
+                <div id="profile"></div>
+                <a id="button" href="addpost.php">Add a Post</a>
+                <a href="">
+                <i class="fa-solid fa-image fa-xl" style="color: #005eff;"></i>
+                </a>
+            </div>
             <div id="stories">
                 <div id="stories-video-div">
                     <div id="video">
+                        <!-- <iframe src="https://assets.pinterest.com/ext/embed.html?id=80150068365267657" frameborder="0" scrolling="no"></iframe> -->
                         <div id="profile">
                         </div>
                         <p id="profilename">
                             Add to Story
                         </p>
                     </div>
-                    <div id="video">
-                    </div>
                 </div>
             </div>
-            <div id="addnewpost">
-                <div id="profile"></div>
-                <a id="button" href="">Add a Post</a>
-                <a href="">
-                <i class="fa-solid fa-image fa-xl" style="color: #005eff;"></i>
-                </a>
-            </div>
+
             <div id="post">
                 <div id="uploader">
                     <div id="profile"></div>
@@ -39,7 +39,9 @@
                 <div id="posttext">
                     <p>Not having fun at all</p>
                 </div>
-                <div id="postimg"></div>
+                <div id="postimg">
+                    <img src="../src/image/2150844459.jpg" alt="">
+                </div>
                 <div id="postreact">
                     <div id="reacts">
                         <p>kyaw and others</p>
@@ -61,6 +63,24 @@
                         <div id="button">
                         <i class="fa-regular fa-share" style="color: #005eff;"></i>
                         <p>Share</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="friendsection">
+                <div id="sub-headings"><i class="fa-solid fa-user-group"></i> <h3>Tun,friend for you</h3></div>
+                <div id="friendsuggestion">
+                    <div id="friend">
+                        <div id="imgdiv">
+                            <img src="../src/image/2150844459.jpg" alt="">
+                        </div>
+                        <div id="friend-info">
+                            <h2>Tun Aung Lin</h2>
+                            <p>2 mutual friends</p>
+                            <div id="button">
+                                <button>Add Friend</button>
+                                <button>Join</button>
+                            </div>
                         </div>
                     </div>
                 </div>
